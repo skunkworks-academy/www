76942d046117f6e69dd6907953563d732dc6ed06
Loyiso <loyisondlovu6@gmail.com>
2025-01-14 09:59:19 +0200

    Added oauth2 ro use outlook as the emailer

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+  
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
