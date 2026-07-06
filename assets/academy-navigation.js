b2b9ea7bb5e39b53b4f80227701922c19d07fe0b
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-07-06 19:28:22 +0200

    Bump academy navigation compatibility loader

--- assets/academy-navigation.js
+++ assets/academy-navigation.js
@@ -5,7 +5,7 @@
 (function () {
   "use strict";
 
-  var version = "2026.07.06.1";
+  var version = "2026.07.06.2";
   var canonicalUrl = "https://skunkworksacademy.com/assets/skunkworks-ui.js?v=" + version;
   var selector = 'script[data-skunkworks-ui="canonical"]';
 
@@ -17,7 +17,7 @@
   script.defer = true;
   script.src = canonicalUrl;
   script.setAttribute("data-skunkworks-ui", "canonical");
-  script.setAttribute("data-skunkworks-global-nav", "v5");
+  script.setAttribute("data-skunkworks-global-nav", "v6");
 
   (document.head || document.documentElement).appendChild(script);
 })();
