46aee1c729175883b36de8d9b4fd4fdd42f034e3
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-07-22 10:54:08 +0200

    fix(pp-101): replace SW mark with Academy logo

--- webinars/workbooks/pp-101/config.js
+++ webinars/workbooks/pp-101/config.js
@@ -11,3 +11,18 @@ window.PP101_CONFIG = Object.freeze({
   storageKey: 'skunkworks-pp-101-workbook-v2',
   maximumResponseCharacters: 4000
 });
+
+const pp101Logo = document.querySelector('.brand .logo');
+if (pp101Logo) {
+  const logoImage = document.createElement('img');
+  logoImage.className = 'logo';
+  logoImage.src = 'https://raw.githubusercontent.com/skunkworks-academy/www/refs/heads/main/images/favicon-black.png';
+  logoImage.alt = 'Skunkworks Academy logo';
+  logoImage.width = 42;
+  logoImage.height = 42;
+  logoImage.style.objectFit = 'contain';
+  logoImage.style.padding = '5px';
+  logoImage.style.background = '#ffffff';
+  logoImage.style.borderRadius = '13px';
+  pp101Logo.replaceWith(logoImage);
+}
